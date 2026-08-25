87e7b5172c4b0ccaac65290c7704df0ad74efd68
Angel Vergara <avergara13@gmail.com>
2026-08-25 16:51:43 -0400

    feat: complete portfolio evidence atlas and final QA

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = Number(process.env.PORT) || 3000;
 app.use(express.static(__dirname, { extensions: ['html'] }));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '404.html'));
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {

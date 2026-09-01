e58ee3db6952317793bdf7c49fb1f532e1d3e4c8
Angel Vergara <avergara13@gmail.com>
2026-09-01 12:23:52 -0400

    feat: reconcile portfolio presentation and Gemini lab

--- patch2.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf-8');
-html = html.replace('  <script src="/static-shell.js"></script>', '  <script src="/static-shell.js"></script>\n  <script src="/chat-app.js"></script>');
-fs.writeFileSync('index.html', html);
